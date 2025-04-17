765e08598ebeddb16dce92b5374dfb3c357705eb
Piory <aiueo8102@gmail.com>
2025-04-18 03:47:32 +0900

    tamagui & @core/presentation を import できるようにした

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -5,24 +5,17 @@
  * @format
  */
 
-import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
+import {ScrollView, StatusBar, StyleSheet, Text, useColorScheme,} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors,} from 'react-native/Libraries/NewAppScreen';
+
+import {createTamagui, TamaguiProvider, View} from 'tamagui';
+import {defaultConfig} from '@tamagui/config/v4'; // for quick config install this
+import {TopScreen} from '@core/presentation';
+
+const config = createTamagui(defaultConfig)
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -73,39 +66,17 @@ function App(): React.JSX.Element {
   const safePadding = '5%';
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+    <TamaguiProvider config={config}>
+      <View style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView style={backgroundStyle}>
+          <TopScreen/>
+        </ScrollView>
+      </View>
+    </TamaguiProvider>
   );
 }
 
