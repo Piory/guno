81eae3e352f511f49c680936c191725344c4585b
Piory <aiueo8102@gmail.com>
2025-04-18 04:30:02 +0900

    iOS のビルド時に、.env の値を参照できるようにした

--- packages/shared/src/env/react-native-config.d.ts
+++ packages/shared/src/env/react-native-config.d.ts
@@ -1,7 +1,7 @@
 declare module 'react-native-config' {
   interface NativeConfig {
     APP_ENV: string;
-    APP_ID_SUFFIX: string;
+    APP_ID: string;
     APP_NAME: string;
     APP_DISPLAY_NAME: string;
     GOOGLE_OAUTH_URL_SCHEME: string;
