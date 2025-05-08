312cf047e2cb004771878136072270073277832a
Piory <aiueo8102@gmail.com>
2025-05-08 11:19:35 +0900

    テストでエラーが発生する問題修正

--- packages/presentation/src/contexts/UseCaseContext/index.test.tsx
+++ packages/presentation/src/contexts/UseCaseContext/index.test.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { useCaseContainer } from '../../composition';
 import { UseCaseProvider, useUseCases } from './index.tsx';
 
